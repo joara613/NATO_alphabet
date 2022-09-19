3bb4373b904e1616deaa593b4e94761c0d643cd5
joara613 <joara613@gmail.com>
2022-09-18 23:40:31 -0600

    add error handler

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 import pandas
 
-#TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
+
 nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
-phonetic_dict= {row.letter: row.code for (index, row) in nato_data.iterrows()}
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("give me a word: ").upper()
-letter_list = [phonetic_dict[letter] for letter in word]
-print(letter_list)
+
+phonetic_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
+
+
+def generate_phonetic():
+    word = input("give me a word: ").upper()
+    try:
+        letter_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(letter_list)
+
+
+generate_phonetic()
